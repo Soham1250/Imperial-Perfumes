0fa32f47223cd7e8b3e3daf21fa8e813c562b322
Soham1250 <soham.pansare1250@gmail.com>
2025-04-14 17:48:04 +0530

    .

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // MongoDB connection configuration
 module.exports = {
   // Use environment variable with fallback for local development
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://sohampansare1250:poVsvANJYEodxZE9@imperialcluster.ya2uoo8.mongodb.net/imperial-perfumes?retryWrites=true&w=majority"
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://sohampansare1250:<>@imperialcluster.ya2uoo8.mongodb.net/imperial-perfumes?retryWrites=true&w=majority"
 };
